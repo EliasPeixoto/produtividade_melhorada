7be63a8a4da29a314d1c44e99c692dbca684e1c8
eliaspeixoto <eliasgdapeixoto@gmail.com>
2025-12-23 17:53:03 -0300

    Adição do evento listener global

--- scripts/behaviors.js
+++ scripts/behaviors.js
@@ -1,20 +1,39 @@
-function generateCard()
-{
+function generateCard() {
     let card_container = document.querySelector('.cards-container');
     let card = document.createElement('li');
-    
+
     card.classList.add('card');
     card.innerHTML = 'Teste';
     card_container.appendChild(card);
 }
-function removeCard(card)
-{
+function removeCard(card) {
     card.remove();
 }
 
-function eventListenerSetup(){
+function eventListenerSetup() {
+    document.addEventListener('click', (event) => {
+        const trigger = event.target.closest('[data-action]');
 
+
+        if (!trigger) return;
+
+
+        const dialogId = trigger.dataset.dialog || trigger.closest('[data-dialog]')?.dataset.dialog;
+
+        if (!dialogId) return;
+
+        const dialog = document.querySelector(`.dialog[data-dialog="${dialogId}"]`);
+
+        const action = trigger.dataset.action;
+
+        const actions = {
+            open: (dialog) => dialog.classList.toggle("closed", false),
+            close: (dialog) => dialog.classList.toggle("closed", true)
+            }
+        actions[action]?.(dialog);
+
+    });
 }
-
+eventListenerSetup();
 generateCard();
 generateCard();
