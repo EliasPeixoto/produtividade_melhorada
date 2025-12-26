278ab8173a40108c2db04b42ba71b6cce3086d80
eliaspeixoto <eliasgdapeixoto@gmail.com>
2025-12-26 17:55:11 -0300

    Reformulação do front-end da Aplicação.

--- scripts/behaviors.js
+++ scripts/behaviors.js
@@ -1,13 +1,25 @@
-function generateCard() {
-    let card_container = document.querySelector('.cards-container');
-    let card = document.createElement('li');
+function generateCard(dialog) {
+    if (dialog.value != '') {
+        let card_container = document.querySelector('.todo-cards-list');
+        let card = document.createElement('li');
 
-    card.classList.add('card');
-    card.innerHTML = 'Teste';
-    card_container.appendChild(card);
+        card.classList.add('card');
+        card.innerHTML = `<p>${dialog.value}</p>`
+        card_container.appendChild(card);
+        dialog.value = '';
+    }
+
 }
 function removeCard(card) {
     card.remove();
+}
+function inputListener() {
+    document.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const trigger = event.target.closest('[data-action]');
+        console.log("Foi");
+
+    })
 }
 
 function eventListenerSetup() {
@@ -28,12 +40,11 @@ function eventListenerSetup() {
 
         const actions = {
             open: (dialog) => dialog.classList.toggle("closed", false),
-            close: (dialog) => dialog.classList.toggle("closed", true)
-            }
+            close: (dialog) => dialog.classList.toggle("closed", true),
+            generate_card: (dialog) => generateCard(dialog),
+        }
         actions[action]?.(dialog);
-
     });
 }
 eventListenerSetup();
-generateCard();
-generateCard();
+inputListener();
